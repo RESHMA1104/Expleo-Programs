2e077917bdaf7385ff78b526b2c1ff91dd915f91
RESHMA1104 <163125056+RESHMA1104@users.noreply.github.com>
2026-03-22 23:16:34 +0530

    Update frequencyExample.java

--- Programs/src/arrayJava/frequencyExample.java
+++ Programs/src/arrayJava/frequencyExample.java
@@ -1,18 +1,28 @@
 package arrayJava;
-import java.util.Scanner;
+
 public class frequencyExample {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array size: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++) {
-        	arr[i] = sc.nextInt();
-        }
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		
-        	}
+
+        int[] arr = {1, 2, 2, 3, 1, 4, 2};
+
+        int n = arr.length;
+        boolean[] visited = new boolean[n];
+
+        for(int i = 0; i < n; i++) {
+
+            if(visited[i] == true)
+                continue;
+
+            int count = 1;
+
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " occurs " + count + " times");
         }
     }
 }
