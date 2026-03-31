98eacf4e7a5275b100e762516dede2b9815f31a8
RESHMMA1104 <resh1104mcdc@gmail.com>
2026-03-31 21:45:59 +0530

    Resolved Conflict

--- conn/src/main/java/Project1/conn/App.java
+++
@@ -1,7 +0,0 @@
-package Project1.conn;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- maven1/src/main/java/expleo/maven1/App.java
+++
@@ -1,7 +0,0 @@
-package expleo.maven1;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
